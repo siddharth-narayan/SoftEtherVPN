b5904d36b522412690203f44658941813a2d821a
Siddharth <siddharth.060605@gmail.com>
2024-06-28 01:30:05 -0400

    Update flake.nix to be much simpler

--- flake.nix
+++ flake.nix
@@ -1,113 +1,49 @@
 { 
   inputs = {
-    openssl.url = "github:siddharth-narayan/openssl-with-providers";
+    openssl-with-oqsprovider.url = "github:siddharth-narayan/openssl-with-providers";
   };
 
-  outputs = { self, nixpkgs, openssl, ... }:
-  let pkgs = import nixpkgs { system = "x86_64-linux"; };
+  outputs = { self, nixpkgs, openssl-with-oqsprovider, ... }:
+  let 
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    softether = pkgs.stdenv.mkDerivation {
+
+      pname = "softether";
+      version = "5.02.5183";
+      src = /home/siddharth/projects/github/SoftEtherVPN;
+      dontFixCmake = true;
+      buildInputs = with pkgs; [ 
+        libsodium 
+        pkgconf 
+        tree 
+        cmake
+        readline
+        ncurses 
+        zlib
+      ];
+
+      preConfigure = ''
+          mkdir -p $out/
+          CMAKE_FLAGS="-DSE_PIDDIR=/tmp/softether -DSE_LOGDIR=/tmp/log/softether -DSE_DBDIR=/tmp/lib/softether" ./configure
+      '';
+      
+      postInstall = ''
+        echo fixing double slash
+        sed 's|//|/|g' $out/lib/pkgconfig/liboqs.pc > $out/lib/pkgconfig/liboqs.pc.tmp
+        cp $out/lib/pkgconfig/liboqs.pc.tmp $out/lib/pkgconfig/liboqs.pc
+      '';
+
+      meta = with pkgs.lib; {
+          description = "An Open-Source Free Cross-platform Multi-protocol VPN Program";
+          homepage = "https://www.softether.org/";
+          license = licenses.asl20;
+          maintainers = [];
+          platforms = [ "x86_64-linux" ];
+      };
+    };
   in
   {
-    packages.x86_64-linux.default = self.packages.x86_64-linux.openssl_with_oqsprovider;
-
-    packages.x86_64-linux.openssl_with_oqsprovider = pkgs.stdenv.mkDerivation {
-
-      pname = "softether";
-      version = "5.02.5183";
-      src = /home/siddharth/projects/github/SoftEtherVPN;
-      dontFixCmake = true;
-      buildInputs = with pkgs; [ 
-        libsodium 
-        pkgconf 
-        tree 
-        cmake
-        openssl.outputs.packages.x86_64-linux.default
-        openssl.outputs.packages.x86_64-linux.oqsprovider-static
-        liboqs
-        readline
-        ncurses 
-        zlib 
-      ];
-
-      preConfigure = ''
-          mkdir -p $out/
-          # CMAKE_INSTALL_LIBEXECDIR="libexec" CMAKE_FLAGS="-DCMAKE_INSTALL_LIBEXECDIR=libexec -DSE_PIDDIR=/tmp/softether -DSE_LOGDIR=/tmp/log/softether -DSE_DBDIR=/tmp/lib/softether" ./configure
-          CMAKE_INSTALL_LIBEXECDIR="libexec" CMAKE_FLAGS="-DCMAKE_INSTALL_LIBEXECDIR=libexec -DCMAKE_BUILD_TYPE=Debug -DSE_PIDDIR=/tmp/softether -DSE_LOGDIR=/tmp/log/softether -DSE_DBDIR=/tmp/lib/softether" ./configure
-      '';
-
-      meta = with pkgs.lib; {
-          description = "An Open-Source Free Cross-platform Multi-protocol VPN Program";
-          homepage = "https://www.softether.org/";
-          license = licenses.asl20;
-          maintainers = [];
-          platforms = [ "x86_64-linux" ];
-      };
-    };
-    
-    packages.x86_64-linux.openssl_default = pkgs.stdenv.mkDerivation {
-
-      pname = "softether";
-      version = "5.02.5183";
-      src = /home/siddharth/projects/github/SoftEtherVPN;
-      dontFixCmake = true;
-      buildInputs = with pkgs; [ 
-        libsodium 
-        pkgconf 
-        tree 
-        cmake
-        pkgs.openssl
-        readline
-        ncurses 
-        zlib 
-      ];
-
-      preConfigure = ''
-          mkdir -p $out/
-          # CMAKE_INSTALL_LIBEXECDIR="libexec" CMAKE_FLAGS="-DCMAKE_INSTALL_LIBEXECDIR=libexec -DSE_PIDDIR=/tmp/softether -DSE_LOGDIR=/tmp/log/softether -DSE_DBDIR=/tmp/lib/softether" ./configure
-          CMAKE_INSTALL_LIBEXECDIR="libexec" CMAKE_FLAGS="-DCMAKE_INSTALL_LIBEXECDIR=libexec -DCMAKE_BUILD_TYPE=Debug -DSE_PIDDIR=/tmp/softether -DSE_LOGDIR=/tmp/log/softether -DSE_DBDIR=/tmp/lib/softether" ./configure
-      '';
-
-      meta = with pkgs.lib; {
-          description = "An Open-Source Free Cross-platform Multi-protocol VPN Program";
-          homepage = "https://www.softether.org/";
-          license = licenses.asl20;
-          maintainers = [];
-          platforms = [ "x86_64-linux" ];
-      };
-    };
-
-    packages.x86_64-linux.openssl_1_1 = pkgs.stdenv.mkDerivation {
-
-      pname = "softether";
-      version = "5.02.5183";
-      src = /home/siddharth/projects/github/SoftEtherVPN;
-      dontFixCmake = true;
-      buildInputs = with pkgs; [ 
-        libsodium 
-        pkgconf 
-        tree 
-        cmake
-        openssl_1_1
-        openssl.outputs.packages.x86_64-linux.oqsprovider-static
-        liboqs
-        readline
-        ncurses 
-        zlib 
-      ];
-
-      preConfigure = ''
-          mkdir -p $out/
-          # CMAKE_INSTALL_LIBEXECDIR="libexec" CMAKE_FLAGS="-DCMAKE_INSTALL_LIBEXECDIR=libexec -DSE_PIDDIR=/tmp/softether -DSE_LOGDIR=/tmp/log/softether -DSE_DBDIR=/tmp/lib/softether" ./configure
-          CMAKE_INSTALL_LIBEXECDIR="libexec" CMAKE_FLAGS="-DCMAKE_INSTALL_LIBEXECDIR=libexec -DCMAKE_BUILD_TYPE=Debug -DSE_PIDDIR=/tmp/softether -DSE_LOGDIR=/tmp/log/softether -DSE_DBDIR=/tmp/lib/softether" ./configure
-      '';
-
-      meta = with pkgs.lib; {
-          description = "An Open-Source Free Cross-platform Multi-protocol VPN Program";
-          homepage = "https://www.softether.org/";
-          license = licenses.asl20;
-          maintainers = [];
-          platforms = [ "x86_64-linux" ];
-      };
-    };
+    packages.x86_64-linux.default = softether.overrideAttrs (old: {buildInputs = old.buildInputs ++ [ pkgs.openssl ]; });
 
     devShells.x86_64-linux.default = pkgs.mkShell {
       packages = with pkgs; [
@@ -115,7 +51,6 @@
         libcxx
         cmake
         gnumake
-        pkgs.openssl
         libiconv
         readline
         ncurses
@@ -123,7 +58,7 @@
         libsodium
         zlib
         # Change this to change what openssl version is built
-        self.packages.x86_64-linux.openssl_default
+        self.packages.x86_64-linux.default
       ];
     };
   };
